d7e1b468f83ace356d420a99034dc67d2a82a8f3
Prakhar Tripathi <prakhar.tripathi@theredpandas.com>
2020-07-31 00:13:10 +0530

    first commit

---
+++ CREDExpandableView/CREDExpandableView.h
@@ -0,0 +1,19 @@
+//
+//  CREDExpandableView.h
+//  CREDExpandableView
+//
+//  Created by Prakhar Tripathi on 31/07/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CREDExpandableView.
+FOUNDATION_EXPORT double CREDExpandableViewVersionNumber;
+
+//! Project version string for CREDExpandableView.
+FOUNDATION_EXPORT const unsigned char CREDExpandableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CREDExpandableView/PublicHeader.h>
+
+
---
+++ CREDExpandableViewExample/CREDExpandableViewExample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CREDExpandableViewExample
+//
+//  Created by Prakhar Tripathi on 31/07/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
